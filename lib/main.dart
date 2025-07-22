6ed1695f8b0d9aef09a21348a3873999cb99cc87
Evelyn Becker <beckernyleve@gmail.com>
2025-07-22 11:47:05 -0300

    configurando firebase e iniciando tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:utilidades/dartAvancado/future/login.dart';
 import 'package:utilidades/dartAvancado/isolate/product_parse.dart';
 import 'package:utilidades/dartAvancado/stream/busca_com_stream.dart';
+import 'package:utilidades/firebase_options.dart';
 import 'package:utilidades/src/app/app_widget.dart';
 import 'package:utilidades/src/views/counter_cubit.dart';
 
-void main() {
-  //runApp(AppWidget());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(AppWidget());
   /* 
   riverpod - é uma biblioteca de gerenciamento de estaado desenvolvida por Rmi Rousselet (criador do provider). Ele foi criado para 
   corrigir limitações do provider e oferecer uma abordagem maais robusta, segura e flexível.
@@ -42,7 +48,7 @@ void main() {
 
   */
 
-  runApp(MyApp());
+  //runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
